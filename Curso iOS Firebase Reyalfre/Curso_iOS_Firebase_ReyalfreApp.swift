139a7420db538ebacbc20e9c1f828e11c6fb7c5b
Alfredo Maldonado <alfredorafael1995@hotmail.com>
2026-03-02 10:07:21 +0100

    Initial Commit

---
+++ Curso iOS Firebase Reyalfre/Curso_iOS_Firebase_ReyalfreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_iOS_Firebase_ReyalfreApp.swift
+//  Curso iOS Firebase Reyalfre
+//
+//  Created by Equipo 8 on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_iOS_Firebase_ReyalfreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
